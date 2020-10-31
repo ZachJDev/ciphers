fae44021b98fbf564330f2b8c2c6c70c132a3356
Zach Johnson <Zachjdev2121@gmail.com>
2020-10-31 07:26:59 -0400

    TYPOSgit status

--- CaesarCipher.js
+++ CaesarCipher.js
@@ -1,4 +1,4 @@
-class CaserCipher {
+class CaesarCipher {
   constructor(rotation) {
     try {
       // confirm is number
@@ -10,6 +10,21 @@ class CaserCipher {
        console.log(e) 
     }
   }
+  _isAlpha(c) {
+     check = c.toUpperCase().charCodeAt(0)
+    return check >= 65 && check <=90
+  }
+  _getAlphaIndex(charCode) {
+    // converts the charCode to the position in the alphabet.
+    if (charCode < 91) return charCode - 65;
+    return this._getAlphaIndex(charCode - 32);
+  }
+  encipher(string) {
+    for(let char in string){
+
+    }
+
+  }
 }
 
-let rot13 = new CaserCipher("s")
+let rot13 = new CaesarCipher("s")
