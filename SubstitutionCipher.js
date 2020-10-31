f4676d6ed5e6df8cf882d00dbcb4b05201b061b3
Zach Johnson <Zachjdev2121@gmail.com>
2020-10-31 07:19:16 -0400

    renamed encodeString variable.

--- SubstitutionCipher.js
+++ SubstitutionCipher.js
@@ -1,34 +1,34 @@
 class SubstitutionCipher {
-  constructor(encodeString) {
+  constructor(key) {
     try {
-      // confirm that the encodeString is actually a string:
-      if (typeof encodeString !== "string")
+      // confirm that the key is actually a string:
+      if (typeof key !== "string")
         throw new TypeError(
-          "encodeString must be of type string. Instead got type " +
-            typeof encodeString
+          "key must be of type string. Instead got type " +
+            typeof key
         );
       //confirm it is the correct length
-      if (encodeString.length !== 26)
-        throw new Error("encodeString must be of length 26");
+      if (key.length !== 26)
+        throw new Error("key must be of length 26");
       //confirm everything is an alphabet character
       // and that it only occurs once.
       const freqList = {};
       for (let i = 0; i < 26; i++) {
-        let char = encodeString.charAt(i).toUpperCase();
+        let char = key.charAt(i).toUpperCase();
         let charCode = char.charCodeAt(0);
         if (charCode < 65 || charCode > 90)
           throw new TypeError(
-            "All members of encodeString must be alphabet characters"
+            "All members of key must be alphabet characters"
           );
         freqList[char] = ++freqList[char] || 1;
         if (freqList[char] > 1)
           throw new Error(
-            "Each letter must occur exactly once in encodeString"
+            "Each letter must occur exactly once in key"
           );
       }
       //
 
-      this._cipher = encodeString.toUpperCase();
+      this._key = key.toUpperCase();
     } catch (e) {
       console.log(e);
     }
@@ -48,7 +48,7 @@ class SubstitutionCipher {
       // if it is an alpha character
       if (alphaCheck >= 65 && alphaCheck <= 90) {
         let alphaIndex = this._getAlphaIndex(char.charCodeAt(0));
-        let cipherLetter = this._cipher[alphaIndex].valueOf();
+        let cipherLetter = this._key[alphaIndex].valueOf();
         //Convert if different cases:
         if (char.charCodeAt(0) >= 97 && cipherLetter.charCodeAt(0) <= 90) {
           // if the input char is lowercase and the cipher is uppercase
@@ -73,7 +73,7 @@ class SubstitutionCipher {
         let char = splitString[i];
       let alphaCheck = char.toUpperCase().charCodeAt(0);
       if (alphaCheck >= 65 && alphaCheck <= 90) { 
-          let index = this._cipher.indexOf(char.toUpperCase())
+          let index = this._key.indexOf(char.toUpperCase())
           let decoded = String.fromCharCode(index + 65)
 
           if(char.charCodeAt(0) >= 97 && char.charCodeAt(0) <= 122) {
